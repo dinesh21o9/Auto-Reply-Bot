086785860f2ce31745f65a0a5e1470d301825283
Dinesh <dineshsurya.2002@gmail.com>
2023-11-22 12:19:28 +0530

    Got Unreplied Mails and done with Interval part

--- index.js
+++ index.js
@@ -9,9 +9,20 @@ const SCOPES = [
     'https://www.googleapis.com/auth/gmail.send',
     'https://www.googleapis.com/auth/gmail.labels',
     'https://mail.google.com/',
-  ];
+];
 
-  // Function to authorize and get authenticated client
+const intervals = [
+    { start: 45000, end: 120000 }, // Interval in milliseconds (45 to 120 seconds)
+    // { start: 60000, end: 180000 }, // Interval in milliseconds (60 to 180 seconds)
+];
+
+// Functino to get random Intervals from given start and end time
+function getRandomInterval(intervals) {
+    const { start, end } = intervals[0];
+    return Math.floor(Math.random() * (end - start + 1) + start);
+}
+
+// Function to authorize and get authenticated client
 async function authorize() {
     const client = await authenticate({
       scopes: SCOPES,
@@ -19,14 +30,39 @@ async function authorize() {
     });
     return client;
 }
-  
 
+// Function to retrieve unreplied messages from the inbox
+async function getUnrepliedMessages(auth) {
+    const gmail = google.gmail({ version: "v1", auth });
+    // Retrieve unread messages from the inbox
+    const response = await gmail.users.messages.list({
+      userId: "me",
+      labelIds: ["INBOX"],
+      q: "is:unread",
+    });
+    return response.data.messages || [];
+}
+
+// Function to run the sequence periodically
+function runSequence(auth) {
+    setInterval(async () => {
+      try {
+        const messages = await getUnrepliedMessages(auth);
+  
+        if (messages && messages.length > 0) {
+            console.log(messages.length);//Checking nnumber of unread messages
+        }
+      } catch (error) {
+        console.error('Error in sequence:', error);
+      }
+    }, getRandomInterval(intervals)); // Random interval between 45 to 120 seconds
+  }
 
 // Main function to start the application
 async function main() {
   try {
     const auth = await authorize();
-    console.log(auth);
+    runSequence(auth);
   } catch (error) {
     console.error('Error starting the app:', error);
   }
